8502548580df08bc3af4f345039ae55c6b587851
oTrolado <gabrielcarvalhocosta@live.com>
2019-04-25 22:06:15 -0300

    logica iniciada

---
+++ controllers/data.js
@@ -0,0 +1,31 @@
+const mjson = [
+    {   "id":"0",
+        "nome":"Stairway To Heaven",
+        "url":"audios/stairway-to-heaven.mp3",
+        "img":"imagens/stairway.jpg"
+    },
+    {   "id":"1",
+        "nome":"Relógio",
+        "url":"audios/Relogio.mp3",
+        "img":"imagens/relogio.jpg"
+    },
+    {   "id":"2",
+        "nome":"Metade Da Estrada",
+        "url":"audios/metade-da-estrada.mp3",
+        "img":"imagens/metade.jpg"
+    },
+    {   "id":"3",
+        "nome":"Era Uma Vez",
+        "url":"audios/era-uma-vez.mp3",
+        "img":"imagens/era-uma-vez.jpg"
+    },
+    {   "id":"4",
+        "nome":"Dancing With a Stranger",
+        "url":"audios/Dancing.mp3",
+        "img":"imagens/stranger.jpg"
+    }
+];
+
+function fetchMusicas(){
+    return mjson;    
+}
--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,7 @@
+
+let musicas = fetchMusicas();
+
+
 function toggleMenu(){
     let menu = document.getElementById('navigator');
     if(!menu.classList.contains('no-nav')) {
@@ -17,3 +21,23 @@ function conserta(){
         menu.classList.replace('no-nav', 'side-nav');
     }
 }
+
+function createLista(dados){
+    let lista = document.getElementById('lista-default');
+    if(dados.length > -1){
+        dados.map(musica => {
+            let item = document.createElement('li');
+            item.classList.add('item-musica');
+            item.setAttribute('name',musica.id);
+            item.innerText = musica.nome;
+            item.setAttribute('onclick','tocarMusica(this.getAttribute("name"))')
+            lista.appendChild(item);
+        });
+    }
+}
+
+function tocarMusica(item){
+    console.log(item);
+}
+
+createLista(musicas);
