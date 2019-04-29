a2ac8f5ab0c58b11deb90e1d281da64384b72725
oTrolado <gabrielcarvalhocosta@live.com>
2019-04-29 00:05:54 -0300

    adding songs

--- controllers/data.js
+++ controllers/data.js
@@ -2,32 +2,53 @@ const mjson = [
     {   "id":"0",
         "nome":"Stairway To Heaven",
         "artista":"Led Zepelin",
+        "album":"",
         "url":"audios/stairway-to-heaven.mp3",
         "img":"imagens/stairway.jpg"
     },
     {   "id":"1",
         "nome":"Relógio",
         "artista":"Gustavo Mioto",
+        "album":"",
         "url":"audios/Relogio.mp3",
         "img":"imagens/relogio.jpg"
     },
     {   "id":"2",
         "artista":"Henrique e Juliano",
         "nome":"Metade Da Estrada",
+        "album":"",
         "url":"audios/metade-da-estrada.mp3",
         "img":"imagens/metade.jpg"
     },
     {   "id":"3",
         "nome":"Era Uma Vez",
         "artista":"Kell Smith",
+        "album":"",
         "url":"audios/era-uma-vez.mp3",
         "img":"imagens/era-uma-vez.jpg"
     },
     {   "id":"4",
         "nome":"Dancing With a Stranger",
         "artista":"Sam Smith",
+        "album":"",
         "url":"audios/Dancing.mp3",
         "img":"imagens/stranger.jpg"
+    },
+    {
+        "id":"5",
+        "nome":"Trem Bala",
+        "artista":"Ana Vilela",
+        "album":"",
+        "url":"audios/trem-bala.mp3",
+        "img":"imagens/trem-bala.jpg"
+    },
+    {
+        "id":"6",
+        "nome":"Um Milhão de Vezes",
+        "artista":"Rafinha Acústico",
+        "album":"",
+        "url":"audios/um-milhao-de-vezes.mp3",
+        "img":"imagens/um-milhao-de-vezes.jpg"
     }
 ];
 
