653c3b89d5337fa7145065e62bdd127111d08ee6
oTrolado <gabriel.costa41@fatec.sp.gov.br>
2019-04-30 15:47:53 -0300

    tentando teste unitario

--- controllers/teste.js
+++ controllers/teste.js
@@ -1,1 +1,20 @@
-console.log('Teste Unitário: Versão 0.1');
+const testeInit = () => {
+    console.log('Teste Unitário: Versão 0.1');
+
+    
+    const removeT = () => {
+        console.log('Remoção de Containers da tela');
+        let input = document.getElementsByClassName('secoes');
+        let output = remove(input);
+        for(i = 0; i< output.lenght; i++){
+            if(output[i].classList.lenght != 2){
+                return console.log('remove() Não passou no teste!');
+            }
+        }
+        return console.log('remove() Passou no teste.');
+    }
+
+
+
+    removeT();
+}
